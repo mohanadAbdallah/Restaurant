13f55fd0a903c2d1eb3af9257b1be59af79553fb
mohanadAbdallah <you@example.com>
2023-08-06 14:40:48 +0300

    API Resource1

--- app/Http/Controllers/Api/V1/Home/RestaurantController.php
+++ app/Http/Controllers/Api/V1/Home/RestaurantController.php
@@ -16,16 +16,13 @@ class RestaurantController extends ApiController
     public function index(): JsonResponse
     {
         return $this->successResponse(RestaurantResource::collection(Restaurant::all()), 200);
-
     }
 
     public function show(Restaurant $restaurant): JsonResponse
     {
         $restaurant->load('categories.items');
 
-        return $this->successResponse([
-            'restaurant'=> new RestaurantResource($restaurant),
-        ],200);
+        return $this->successResponse(new RestaurantResource($restaurant),200);
     }
 
 }
